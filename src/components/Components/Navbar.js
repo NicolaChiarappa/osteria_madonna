55e15ea45a97f06aedf1666f39a34cff6d448495
Nick <nicolachiarappa2001@gmail.com>
2023-03-30 19:31:06 +0200

    added hero background image

--- src/components/Components/Navbar.js
+++ src/components/Components/Navbar.js
@@ -4,22 +4,34 @@ import HStack from "../Layout/HStack";
 import VStack from "../Layout/VStack";
 
 import { imageConfigDefault } from "next/dist/shared/lib/image-config";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <HStack style='items-center bg-black'>
-      <Image
-        priority
-        src='/logo.svg'
-        alt=''
-        width={100}
-        height={100}
-        className=''
-      />
-
-      <div className='border-solid border-black border-2 flex flex-row items-center bg-slate-600'>
-        <h1>Ciao</h1>
+    <HStack style='items-center bg-success justify-between sti'>
+      <div className='p-3'>
+        <Image
+          priority
+          src='/logo.svg'
+          alt=''
+          width={150}
+          height={150}
+          className='fill-blue-500'
+        />
       </div>
+
+      <div className=' flex flex-row items-center max-sm:hidden'>
+        <h1 className='text-4xl font-sans '>Ciao</h1>
+      </div>
+      <HStack className='items-center sm:hidden mr-5 justify-start'>
+        <Button
+          onClick={() => {
+            alert("mobile");
+          }}
+        >
+          alert
+        </Button>
+      </HStack>
     </HStack>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#385738",
+      secondary: "#fbbe0e",
+      white: "#fdfdfd",
+      success: "#917639",
+    },
   },
   plugins: [],
 };
