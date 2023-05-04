b03bc4929bbc83436fc54160d1d576777225a874
Nick <nicolachiarappa2001@gmail.com>
2023-05-04 13:36:48 +0200

    improved seo on navabr items

--- src/components/Components/Navbar.js
+++ src/components/Components/Navbar.js
@@ -17,8 +17,8 @@ const Navbar = () => {
 
   return (
     <>
-      <HStack style='sticky top-0 w-full  text-white z-30 justify-between  h-fit bg-secondary items-center py-3 '>
-        <Link href='/' className='ml-7'>
+      <nav className='flex flex-row sticky top-0 w-full  text-white z-30 justify-between  h-fit bg-secondary items-center py-3 '>
+        <Link href='/' className='ml-7' title='Home'>
           <Image alt='' src='/logo.svg' width={100} height={100}></Image>
         </Link>
         <NavItems></NavItems>
@@ -38,7 +38,7 @@ const Navbar = () => {
             }
           ></Button>
         </HStack>
-      </HStack>
+      </nav>
       <Menu show={show} setShow={setShow} blur={blur}></Menu>
     </>
   );
@@ -47,30 +47,40 @@ const Navbar = () => {
 const NavItems = () => {
   return (
     <HStack style='items-center flex max-lg:hidden space-x-10 mr-10 '>
-      <Link href='/menu'>
-        <Heading as='h1' size='3xl'>
-          Menu
-        </Heading>
+      <Link
+        title='Il menu 2023'
+        href='/menu'
+        className='font-semibold font-miofont text-black  text-center  text-xl'
+      >
+        Menu
       </Link>
-      <Link href='/menu_gruppi'>
-        <Heading as='h1' size='3xl'>
-          Menu gruppi
-        </Heading>
+      <Link
+        title='I menu per i gruppi'
+        href='/menu_gruppi'
+        className='font-semibold font-miofont text-black  text-center  text-xl'
+      >
+        Menu Gruppi
       </Link>
-      <Link href='/cantina'>
-        <Heading as='h1' size='3xl'>
-          La cantina
-        </Heading>
+      <Link
+        title='I nostri vini'
+        href='/cantina'
+        className='font-semibold font-miofont text-black  text-center  text-xl'
+      >
+        La cantina
       </Link>
-      <Link href='/about'>
-        <Heading as='h1' size='3xl'>
-          Chi siamo
-        </Heading>
+      <Link
+        title='Scopri chi siamo'
+        href='/about'
+        className='font-semibold font-miofont text-black  text-center  text-xl'
+      >
+        Chi siamo
       </Link>
-      <Link href='/prenota'>
-        <Heading as='h1' size='3xl'>
-          Prenota
-        </Heading>
+      <Link
+        title='Prenota un tavolo'
+        href='/prenota'
+        className='font-semibold font-miofont text-black  text-center  text-xl'
+      >
+        Prenota
       </Link>
     </HStack>
   );
