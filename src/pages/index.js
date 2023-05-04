8140acf3295868a101858db91c1f218ed0b99c47
Nick <nicolachiarappa2001@gmail.com>
2023-05-04 14:41:34 +0200

    added google search console html file

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/osteria.ico' />
       </Head>
+
       <Navbar></Navbar>
       <Hero></Hero>
       <Motto></Motto>
