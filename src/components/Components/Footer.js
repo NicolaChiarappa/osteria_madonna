2620ea9eb27b0a19526809690a40b41ba52279ce
Nick <nicolachiarappa2001@gmail.com>
2023-05-04 14:12:46 +0200

    added sitemap and robots.txt and social links on footer

--- src/components/Components/Footer.js
+++ src/components/Components/Footer.js
@@ -2,6 +2,9 @@ import React from "react";
 import VStack from "../Layout/VStack";
 import HStack from "../Layout/HStack";
 import Image from "next/image";
+import Link from "next/link";
+import Button from "../Bricks/Button";
+import { IoCall, IoLogoInstagram } from "react-icons/io5";
 
 const Footer = () => {
   return (
@@ -16,11 +19,34 @@ const Footer = () => {
         ></Image>
       </VStack>
 
-      <VStack style='justify-center space-y-3'>
+      <VStack style='justify-center space-y-3 items-end'>
         <p className='text-white font-miofont font-light'>
           Pavia, Via dei liguri,28
         </p>
-        <p className='text-white font-miofont font-light'>0382 302833</p>
+        <HStack style='space-x-2 items-center'>
+          <Button icon={<IoCall color='white' size={20} />}></Button>
+          <Link href='tel:+390382302833'>
+            <p className='text-white font-miofont font-light'>0382 302833</p>
+          </Link>
+        </HStack>
+        <HStack style='space-x-3'>
+          <Button
+            icon={
+              <IoLogoInstagram
+                color='white'
+                size='26'
+                onClick={() => {
+                  open(
+                    "https://instagram.com/osteriadellamadonnapavia?igshid=YmMyMTA2M2Y="
+                  );
+                }}
+              ></IoLogoInstagram>
+            }
+          ></Button>
+          <p className='text-white text-sm font-miofont font-thin'>
+            osteriadellamadonnapavia
+          </p>
+        </HStack>
       </VStack>
     </HStack>
   );
